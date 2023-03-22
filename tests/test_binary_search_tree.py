b0843d0490ae03ec8f1bfce85d5f2280785b2c62
kirillskyeng <kirill.netreba@skyeng.ru>
2023-03-22 15:13:41 +0300

    Add tests for insert for 100% coverage

--- tests/test_binary_search_tree.py
+++ tests/test_binary_search_tree.py
@@ -31,3 +31,17 @@ def test_bst_insert_right(bst_root):
     """Проверяем добавление вправо от вершины BST."""
     bst_root.insert({'id': 50})
     assert bst_root.root.right.data['id'] == 50
+
+
+def test_bst_insert_left_left(bst_root):
+    """Проверяем добавление два раза влево от вершины BST."""
+    bst_root.insert({'id': 30})
+    bst_root.insert({'id': 25})
+    assert bst_root.root.left.left.data['id'] == 25
+
+
+def test_bst_insert_right_right(bst_root):
+    """Проверяем добавление два раза вправо от вершины BST."""
+    bst_root.insert({'id': 50})
+    bst_root.insert({'id': 60})
+    assert bst_root.root.right.right.data['id'] == 60
